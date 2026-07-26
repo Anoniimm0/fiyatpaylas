b73eb9a504e07dc8b2c7f04b09e298c4dbaf9e67
anoniimm0 <burakyiilmazz@gmail.com>
2026-07-26 15:01:37 +0300

    Admin giriş sistemi ve veritabanı altyapısı

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template, request
-
-print("APP DOSYASI ÇALIŞTI")
+from flask import Flask, render_template, request, redirect, url_for, flash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from database import db
 from models import User, Post
 
+print("APP DOSYASI ÇALIŞTI")
+
 app = Flask(__name__)
+
+app.secret_key = "fiyatpaylas_v1"
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///fiyatpaylas.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -13,21 +16,52 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
 
+def create_admin():
+    admin = User.query.filter_by(kullanici_adi="admin").first()
+
+    if not admin:
+        admin = User(
+            ad_soyad="Yönetici",
+            kullanici_adi="admin",
+            sifre=generate_password_hash("123456"),
+            rol="admin"
+        )
+
+        db.session.add(admin)
+        db.session.commit()
+
+        print("Admin hesabı oluşturuldu.")
+
+
 @app.route("/", methods=["GET", "POST"])
 def login():
 
-    print("İstek geldi:", request.method)
-
     if request.method == "POST":
-        print("POST ÇALIŞTI")
 
-        username = request.form.get("username")
-        password = request.form.get("password")
+        username = request.form["username"]
+        password = request.form["password"]
 
-        print("Kullanıcı:", username)
-        print("Şifre:", password)
+        user = User.query.filter_by(
+            kullanici_adi=username
+        ).first()
+
+        if user:
+
+            if check_password_hash(user.sifre, password):
+
+                if user.rol == "admin":
+                    return redirect("/admin")
+
+                return "Kullanıcı Paneli"
+
+        flash("Kullanıcı adı veya şifre hatalı.")
 
     return render_template("login.html")
+
+
+@app.route("/admin")
+def admin_panel():
+    return render_template("admin.html")
 
 
 @app.route("/test")
@@ -39,5 +73,6 @@ if __name__ == "__main__":
 
     with app.app_context():
         db.create_all()
+        create_admin()
 
     app.run(debug=True)
